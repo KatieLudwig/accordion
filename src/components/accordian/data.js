bb2de292d52ad74305e01a91c2b445fb9bd3d478
Katie Ludwig <katierludwig@gmail.com>
2025-03-24 20:03:23 -0700

    wip

--- src/components/accordian/data.js
+++ src/components/accordian/data.js
@@ -1,35 +1,41 @@
-const data = [
+const data =[
     {
-        "username": "CaptainMarvelous",
-        "avatar": "🦸‍♂️",
-        "status": "Flying high in the sky!",
-        "lastMessage": "Just saved the day again!"
+      "id": 1,
+      "username": "CaptainMarvelous",
+      "avatar": "🦸‍♂️",
+      "status": "Flying high in the sky!",
+      "lastMessage": "Just saved the day again!"
     },
     {
-        "username": "MysticMerlin",
-        "avatar": "🧙‍♂️",
-        "status": "Conjuring spells and sipping tea.",
-        "lastMessage": "Abracadabra! 🪄"
+      "id": 2,
+      "username": "MysticMerlin",
+      "avatar": "🧙‍♂️",
+      "status": "Conjuring spells and sipping tea.",
+      "lastMessage": "Abracadabra! 🪄"
     },
     {
-        "username": "GalacticGamer",
-        "avatar": "👾",
-        "status": "Exploring new galaxies.",
-        "lastMessage": "Just defeated the final boss!"
+      "id": 3,
+      "username": "GalacticGamer",
+      "avatar": "👾",
+      "status": "Exploring new galaxies.",
+      "lastMessage": "Just defeated the final boss!"
     },
     {
-        "username": "NatureNinja",
-        "avatar": "🌿",
-        "status": "Blending in with the forest.",
-        "lastMessage": "Silent as a leaf 🍃"
+      "id": 4,
+      "username": "NatureNinja",
+      "avatar": "🌿",
+      "status": "Blending in with the forest.",
+      "lastMessage": "Silent as a leaf 🍃"
     },
     {
-        "username": "TechTinkerer",
-        "avatar": "🤖",
-        "status": "Building the future, one byte at a time.",
-        "lastMessage": "Just coded a new AI!"
+      "id": 5,
+      "username": "TechTinkerer",
+      "avatar": "🤖",
+      "status": "Building the future, one byte at a time.",
+      "lastMessage": "Just coded a new AI!"
     }
-];
+  ]
+  
 
 export default data; 
   
--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -1,6 +1,20 @@
+//single slection
+//multiple selection
 
+import { useState } from "react";
 
 export default function Accordian() {
     
-    return <div>Accordian</div>;
+    const [selected, setSelected] = useState(null);
+
+    return <div className="wrapper">
+        <div className="accordian">
+            {
+                data && data.length > 0 ?
+                data.map(dataItem-> <div className="">)
+                : <div>No data found</div>
+            }
+        </div>
+
+    </div>;
 }
