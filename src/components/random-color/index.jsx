85d9f68fa972d753152b1832a299adee33de98bb
Katie Ludwig <katierludwig@gmail.com>
2025-04-14 17:39:25 -0700

    setting up

--- src/components/random-color/index.jsx
+++ src/components/random-color/index.jsx
@@ -1,8 +1,9 @@
 
-
 export default function RandomColor() {
-    
-    return <div className="container">
 
+    return <div className="container">
+        <button>Create HEX Color</button>
+        <button>Create RGB Color</button>
+        <button>Generate Random Color</button>
     </div>
 }
