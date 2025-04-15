f6982db26422f719c47faea89306f59db5b59354
Katie Ludwig <katierludwig@gmail.com>
2025-04-14 18:07:34 -0700

    update to work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
-import Accordian from './components/accordian'
+import Accordian from './components/accordian';
+import RandomColor from './components/random-color';
 
 function App() {
   return (
     <div className="App">
       {/* Accordion Component */}
-      <Accordian/>
+      <Accordian />
+      
+      {/* Random color component */}
+      <RandomColor/>
     </div>
   );
 }
--- src/components/random-color/index.jsx
+++ src/components/random-color/index.jsx
@@ -2,16 +2,19 @@ import { useState } from "react"
 
 export default function RandomColor() {
     const [typeOfColor, setTypeOfColor] = useState('hex');
-    const [color, setColor] = useState('#000')
+    const [color, setColor] = useState('#000');
 
-    return <div style={{
-        width: '100w',
-        height: '100vh',
-        background: color,
-    }}
-    >
-        <button>Create HEX Color</button>
-        <button>Create RGB Color</button>
-        <button>Generate Random Color</button>
-    </div>
+    return (
+        <div
+            style={{
+                width: '100w',
+                height: '100vh',
+                background: color,
+            }}
+        >
+            <button>Create HEX Color</button>
+            <button>Create RGB Color</button>
+            <button>Generate Random Color</button>
+        </div>
+    )
 }
