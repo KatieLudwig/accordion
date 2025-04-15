5d52cb37a31f65273432eb0a78128a2541f55ad3
Katie Ludwig <katierludwig@gmail.com>
2025-04-14 17:58:10 -0700

    updated index.jsx

--- src/components/random-color/index.jsx
+++ src/components/random-color/index.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react"
 
 export default function RandomColor() {
+    const [typeOfColor, setTypeOfColor] = useState('hex');
+    const [color, setColor] = useState('#000')
 
-    return <div className="container">
+    return <div style={{
+        width: '100w',
+        height: '100vh',
+        background: color,
+    }}
+    >
         <button>Create HEX Color</button>
         <button>Create RGB Color</button>
         <button>Generate Random Color</button>
