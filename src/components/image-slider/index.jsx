6184774f57b53baf6a0687af177bf8957c5ff8c4
Katie Ludwig <katierludwig@gmail.com>
2025-04-28 17:50:24 -0700

    wip

--- src/components/image-slider/index.jsx
+++ src/components/image-slider/index.jsx
@@ -4,9 +4,25 @@ export default function ImageSlider({ url, limit }) {
     
     const [imgages, setImages] = useState([]);
     const [currentSlide, setCurrentSlide] = useState(0);
+    const [errorMsg, setErrorMsg] = useState(null)
+    
+    async function fetchImages(getUrl) {
+        try {
+
+            const response = await fetch(getUrl);
+            const data = await response.json();
+
+            if (data) {
+                setImages
+            }
+
+        } catch(e) {
+            setErrorMsg(e.message);
+        }
+    }
 
     useEffect(() => {
-        
+        if(url !== '') fetchImages(url)
     },[url])
 
     return <div className="container"></div>;
