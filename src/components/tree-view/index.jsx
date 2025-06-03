a39811c8a01fee6b8a89d48700f15f4fe0f3794c
Katie Ludwig <katierludwig@gmail.com>
2025-06-02 20:07:38 -0700

    wip

--- src/components/tree-view/index.jsx
+++ src/components/tree-view/index.jsx
@@ -1,9 +1,8 @@
-export default function TreeView({ menurs = [] }) {
+import MenuList from './menu-list';
+export default function TreeView({ menus = [] }) {
     
 
     return <div className='tree-view-container'>
-        {
-            
-        }
+        <MenuList list={menus} />
     </div>
 }
