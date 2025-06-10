a9ba1b4dddda89027ecd2d691b4f195a22fc1664
Katie Ludwig <katierludwig@gmail.com>
2025-06-09 22:34:35 -0700

    wip

--- src/components/tree-view/menu-item.jsx
+++ src/components/tree-view/menu-item.jsx
@@ -1,13 +1,22 @@
+import { useState } from 'react';
 import MenuList from './menu-list';
 
-export default function MenuItem({ item }) {
-    return <li>
-        <p>{item.label}</p>
 
+export default function MenuItem({ item }) {
+
+    const [displayCurrentChildren, setDisplayCurrentChildren] = useState({});
+
+    return <li>
+        <div>
+            <p>{item.label}</p>
+            {
+                item && item.children && item.children.length ? <span>+</span> : null
+            }
+        </div>
         {
             item && item.children && item.children.length > 0 ?
-            <MenuList list={item.children} />
-            : null
+                <MenuList list={item.children} />
+                : null
         }
     </li>
 }
