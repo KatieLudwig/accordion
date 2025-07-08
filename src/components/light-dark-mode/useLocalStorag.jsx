99e86e629f4db487a05d10a2bc4d2d798f981443
Katie Ludwig <katierludwig@gmail.com>
2025-07-07 19:48:13 -0700

    wip

---
+++ src/components/light-dark-mode/useLocalStorag.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export default function useLocalStorage(key, defaultValue) {
+    
+    const [value, setValue] = useState(() => {
+        let currentValue;
+
+        try {
+
+        } catch (error) {
+            console.log(error);
+            currentValue = defaultValue
+        }
+    })
+}
