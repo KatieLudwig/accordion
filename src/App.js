329b01e19a6f6752efd0a4f578751a3e18797c98
Katie Ludwig <katierludwig@gmail.com>
2025-07-14 20:05:14 -0600

    wip

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import TreeView from './components/tree-view';
 import menus from './components/tree-view/data';
 import QRCodeGenerator from './components/qr-code-generator';
 import LightDarkMode from './components/light-dark-mode';
+import ScrollIndicator from './components/scroll-indicator';
+
 function App() {
   return (
     <div className="App">
@@ -36,8 +38,11 @@ function App() {
       {/* QR Code generator
       <QRCodeGenerator /> */}
 
-      {/*Light Dark Mode */}
-      <LightDarkMode />
+      {/*Light Dark Mode 
+      <LightDarkMode /> */}
+
+      {/* Scroll inicatior component */}
+      <ScrollIndicator url={'https://dummyjson.com/products?limit=100'}/>
     </div>
   );
 }
--- src/components/scroll-indicator/index.js
+++ src/components/scroll-indicator/index.js
@@ -1,9 +1,32 @@
+import { useEffect, useState } from 'react';
 
 
+export default function ScrollIndicator({ url }) {
 
-export default function ScrollIndicator(){
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    async function fetchData(getUrl) {
+        try {
+            setLoading(true);
+            const response = await fetch(getUrl);
+            const data = await response.json();
+
+            console.log(data);
+
+        } catch (e) {
+            console.log(e);
+            setErrorMessage(e.message)
+        }
+    }
+
+    useEffect(() => {
+
+        fetchData(url);
+    }, [url]);
 
     return <div>
-        
+
     </div>
 }
