483c1c2f745d1fed95c914b3a48e441938efffce
Katie Ludwig <katierludwig@gmail.com>
2025-07-28 18:25:49 -0600

    fixing bugs

--- src/components/custom-tabs/tab-test.jsx
+++ src/components/custom-tabs/tab-test.jsx
@@ -10,17 +10,17 @@ export default function TabTest() {
 
     const tabs = [
         {
-            label: 'Tab 1',
-            content: <div>This is content for Tab 1</div>
+            label : 'Tab 1',
+            content : <div>This is content for Tab 1</div>
         },
         {
-            label: 'Tab 2',
-            content: <div>This is content for Tab 2</div>
+            label : 'Tab 2',
+            content : <div>This is content for Tab 2</div>
         },
         {
-            label: 'Tab 3',
-            content: <RandomComponent/>
-        }
+            label : 'Tab 3',
+            content : <RandomComponent/>
+        },
     ]
 
     function handleChange(currentTabIndex) {
--- src/components/custom-tabs/tabs.jsx
+++ src/components/custom-tabs/tabs.jsx
@@ -2,8 +2,6 @@ import { useState } from 'react';
 import './tabs.css'; 
 
 export default function Tabs({ tabsContent, onChange }) {
-
-
     const [currentTabIndex, setCurrentTabIndex] = useState(0);
 
     function handleOnClick(getCurrentIndex) {
@@ -14,13 +12,13 @@ export default function Tabs({ tabsContent, onChange }) {
     return <div className="wrapper">
         <div className="heading">
             {tabsContent.map((tabItem, index) => (
-                <div className={`tab-item ${currentTabIndex === index ? 'active' : ''}`} onClick={()=> handleOnClick(index) } key={tabItem.lael}>
+                <div className={`tab-item ${currentTabIndex === index ? 'active' : ''}`} onClick={()=> handleOnClick(index)} key={tabItem.label}>
                     <span className="label">{tabItem.label}</span>
                 </div>
             ))}
         </div>
-        <div className="content" style={{color :'red'}}>
-            {tabsContent[currentTabIndex] && tabsContent[currentTabIndex.content]}
+        <div className="content" style={{ color: 'red' }} >
+            {tabsContent[currentTabIndex] && tabsContent[currentTabIndex].content}
         </div>
     </div>
 }
