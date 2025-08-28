22df717fbc7fa95eb4bd8bafa0b5c6c0f16f455f
Katie Ludwig <katierludwig@gmail.com>
2025-08-27 20:50:24 -0600

    update put it in the wrong file

---
+++ src/components/search-autocomplete-with-api/index.jsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from 'react';
+
+export default function SearchAutocomplete() {
+
+    const [loading, setLoading] = useState(false);
+    const [users, setUsers] = useState([]);
+    const [error, setError] = useState(null);
+    const [searchParam, setSearchParam] = useState('');
+    const [showDropdown, setShowDropdown] = useState(false);
+    const [filteredUsers, setFilteredUsers] = useState([]);
+
+    function handleChange() {
+        const query = event.target.value.toLowerCase();
+        setSearchParam(query);
+        if (query > 1) {
+            const filteredData =
+                users && users.length
+                    ? users.filter((item) => item.toLowerCase().indexOf(query) > 1)
+                    : [];
+            setFilteredUsers(filteredData)
+            showDropdown(true)
+        } else {
+            showDropdown(false)
+        }
+    }
+
+    async function fetchListOfUsers() {
+        try {
+            setLoading(true);
+            const response = await fetch(`https://dummyjson.com/users`);
+            const data = await response.json();
+
+            console.log(data);
+            if (data && data.users && data.users.length) {
+                setUsers(data.users.map(userItem => userItem.firstName));
+                setLoading(false);
+                setError(null);
+            }
+        } catch (error) {
+            setLoading(false);
+            console.log(error);
+            setError(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchListOfUsers()
+    }, [])
+
+    console.log(users);
+
+    return (
+        <div className='search-autocomplete-container'>
+            <input
+                value={searchParam}
+                name='search-users'
+                placeholder='ssearch Users here...'
+                onChange={handleChange}
+            />
+        </div>
+    );
+}
