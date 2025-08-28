a1554022b388694b9a52fb3a27f01a24534f4618
Katie Ludwig <katierludwig@gmail.com>
2025-08-27 20:30:19 -0600

    wip

--- src/components/search-autocomplete-with-api/suggestions.jsx
+++ src/components/search-autocomplete-with-api/suggestions.jsx
@@ -1,6 +1,26 @@
 import { useEffect, useState } from 'react';
 
 export default function SearchAutocomplete() {
+
+    const [loading, setLoading] = useState(false);
+    const [users, setUsers] = useState([]);
+    const [error, setError] = useState(null);
+
+    async function fetchListOfUsers() {
+        try {
+            const response = await fetch(`https://dummyjson.com/users`);
+            const data = await response.json();
+            
+        } catch (error) {
+            console.log(error);
+            setError(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchListOfUsers()
+    }, [])
+
     return (
         <div className='search-autocomplete-container'>
             <input name='search-users' placeholder='ssearch Users here...' />
