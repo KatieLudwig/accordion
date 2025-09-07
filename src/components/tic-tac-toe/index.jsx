9d83a8a6452f617a9319192e4bf28c516dab47f4
Katie Ludwig <katierludwig@gmail.com>
2025-09-07 16:36:42 -0600

    wip

--- src/components/tic-tac-toe/index.jsx
+++ src/components/tic-tac-toe/index.jsx
@@ -1,13 +1,16 @@
-
+import { useState } from 'react';
+import './styles.css'
 // 0 1 2
 // 3 4 5
 // 6 7 8
 
 function Square({value}){
-    return <button>{value}</button>
+    return <button className="square">{value}</button>
 }
 
 export default function TicTacToe() {
+
+    const [squares, setSquares] = useState(Array(9).fill(''))
     return (
         <div className="tic-tac-toe-container">
             <div className="row">
