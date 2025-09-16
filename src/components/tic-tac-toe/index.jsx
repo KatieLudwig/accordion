e3f85d201d06ff63d4e0ad2fba2f3925810c15f4
Katie Ludwig <katierludwig@gmail.com>
2025-09-15 19:24:26 -0600

    wip

--- src/components/tic-tac-toe/index.jsx
+++ src/components/tic-tac-toe/index.jsx
@@ -24,19 +24,19 @@ export default function TicTacToe() {
     return (
         <div className="tic-tac-toe-container">
             <div className="row">
-                <Square onClick={() => handleClick(0)} />
-                <Square onClick={() => handleClick(1)} />
-                <Square onClick={() => handleClick(2)} />
+                <Square value={squares[0]} onClick={() => handleClick(0)} />
+                <Square value={squares[1]} onClick={() => handleClick(1)} />
+                <Square value={squares[2]} onClick={() => handleClick(2)} />
             </div>
             <div className="row">
-                <Square onClick={() => handleClick(3)} />
-                <Square onClick={() => handleClick(4)} />
-                <Square onClick={() => handleClick(5)} />
+                <Square value={squares[3]} onClick={() => handleClick(3)} />
+                <Square value={squares[4]} onClick={() => handleClick(4)} />
+                <Square value={squares[5]} onClick={() => handleClick(5)} />
             </div>
             <div className="row">
-                <Square onClick={() => handleClick(6)} />
-                <Square onClick={() => handleClick(7)} />
-                <Square onClick={() => handleClick(8)} />
+                <Square value={squares[6]} onClick={() => handleClick(6)} />
+                <Square value={squares[7]} onClick={() => handleClick(7)} />
+                <Square value={squares[8]} onClick={() => handleClick(8)} />
             </div>
         </div>
     )
