76e88d33ecf4992f6775be60fbb4e9a9e269d8d2
Katie Ludwig <katierludwig@gmail.com>
2025-09-15 19:11:26 -0600

    wip

--- src/components/tic-tac-toe/index.jsx
+++ src/components/tic-tac-toe/index.jsx
@@ -4,19 +4,21 @@ import './styles.css'
 // 3 4 5
 // 6 7 8
 
-function Square({value}){
-    return <button className="square">{value}</button>
+function Square({value, onClick }){
+    return <button onClick={onClick} className="square">{value}</button>
 }
 
 export default function TicTacToe() {
 
-    const [squares, setSquares] = useState(Array(9).fill(''))
+    const [squares, setSquares] = useState(Array(9).fill(''));
+    const [isXTurn, setIsXTurn] = useState(true);
+
     return (
         <div className="tic-tac-toe-container">
             <div className="row">
-                <Square/>
-                <Square/>
-                <Square/>
+                <Square onClick={() => handleClick(0)} />
+                <Square onClick={() => handleClick(1)}/>
+                <Square onClick={() => handleClick(2)}/>
             </div>
             <div className="row">
                 <Square/>
