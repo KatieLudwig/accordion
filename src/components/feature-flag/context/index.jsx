bcfab1db7874014ec16706234334f4bc29dda1c1
Katie Ludwig <katierludwig@gmail.com>
2025-09-21 17:15:14 -0600

    wip

--- src/components/feature-flag/context/index.jsx
+++ src/components/feature-flag/context/index.jsx
@@ -1,2 +1,10 @@
+import { createContext } from "react";
 
+export const FeatureFlagsContext = createContext(null);
 
+export default function FeatureFlagGloblState({ children }) {
+
+    return <FeatureFlagsContext.Provider value={{}}>
+        {children}
+    </FeatureFlagsContext.Provider>
+}
---
+++ src/components/feature-flag/index.jsx
@@ -0,0 +1,7 @@
+
+
+export default function FeatureFlag() {
+    return <div>
+        <h1>Feature Flags</h1>
+    </div>
+}
