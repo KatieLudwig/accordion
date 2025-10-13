d00e5e33d94900ea4a767da3913a5663ad5c5da2
Katie Ludwig <katierludwig@gmail.com>
2025-10-13 15:42:47 -0600

    wip

--- src/components/use-fetch/test.jsx
+++ src/components/use-fetch/test.jsx
@@ -13,6 +13,9 @@ export default function UseFetchHookTest() {
             pending ? <h3>Pending ! Please wait</h3> : null
         }
         {
+            error ? <h3>{error}</h3> : null
+        }
+        {
             data && data.products && data.products.length ?
                 data.products.map(productsItem => <p key={productsItem.key}>{productsItem.title}</p>) : null
         }
