6abd2cb03d0db5fdc7a75316d7cab493a9328610
Katie Ludwig <katierludwig@gmail.com>
2025-10-13 15:57:26 -0600

    wip

--- src/components/feature-flag/context/index.jsx
+++ src/components/feature-flag/context/index.jsx
@@ -1,31 +1,13 @@
-// import { createContext, useEffect, useState } from "react";
-// import featureFlagsDataServiceCall from "../data";
+import { createContext } from "react";
 
-// export const FeatureFlagsContext = createContext(null);
 
-// export default function FeatureFlagGlobalState({ children }) {
-//     const [loading, setLoading] = useState(false);
-//     const [enabledFlags, setEnabledFlags] = useState({});
+export const FeatureFlagsContext = createContext(null);
 
-//     async function fetchFeatureFlags() {
-//         try {
-//             setLoading(true);
-//             //original service call
-//             const response = await featureFlagsDataServiceCall();
-//             setEnabledFlags(response);
-//             setLoading(false);
-//         } catch (error) {
-//             console.log(error);
-//             setLoading(false)
-//             throw new Error(error);
-//         }
-//     }
-
-//     useEffect(() => {
-//         fetchFeatureFlags();
-//     }, []);
-
-//     return <FeatureFlagsContext.Provider value={{ loading, enabledFlags }}>
-//         {children}
-//     </FeatureFlagsContext.Provider>
-// }
+export default function FeatureFlagGlobalState({ children }) {
+    
+    return (
+        <FeatureFlagsContext.Provider value={{}}>
+            {children}
+        </FeatureFlagsContext.Provider>
+    );
+}
