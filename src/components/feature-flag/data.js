abfe070bd4d6d6d624ca103b576878601d8a904b
Katie Ludwig <katierludwig@gmail.com>
2025-10-13 15:54:20 -0600

    wip

--- src/components/feature-flag/data.js
+++ src/components/feature-flag/data.js
@@ -1,19 +1,19 @@
 
-// const dummyApiResponse = {
-//     showLightAndDarkMode: true,
-//     showTicTacToeBoard: true,
-//     showRandomColorGenerator: true,
-//     showAccoordian: true,
-//     showTreeView: true,
-//     showTabs : true
-// }
 
-// function featureFlagsDataServiceCall() {
+const dummyApiResponse = {
+    showLightAndDarkMode: false,
+    showTicTacToeBoard: true,
+    showRandomColorGenerator: true,
+    showAccordian: false,
+    showTreeView: true,
+};
+
+function featureFlagsDataServiceCall() {
     
-//     return new Promise((resolve, reject) => {
-//         if (dummyApiResponse) setTimeout(resolve(dummyApiResponse), 500);
-//         else reject("Some error occured! Please try again");
-//     });
-// }
+    return new Promise((resolve, reject) => {
+        if (dummyApiResponse) setTimeout(resolve(dummyApiResponse), 500);
+        else reject('Some error occured! Please try again')
+    })
+}
 
-// export default featureFlagsDataServiceCall;
+export default featureFlagsDataServiceCall;
