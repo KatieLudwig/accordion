3a9bf93a5adc69279e56a1b4091234c9a908b236
Katie Ludwig <katierludwig@gmail.com>
2025-11-03 15:22:53 -0800

    wip

---
+++ src/components/use-window-resize/test.jsx
@@ -0,0 +1,15 @@
+import useWindowResize from ".";
+
+export default function UseWindowResizeTest() {
+    const windowSize = useWindowResize();
+    const { width, height } = windowSize;
+    return <diiv>
+        <h1>Use Window Resize Hook</h1>
+        <p>
+            Width is {width}
+        </p>
+        <p>
+            Height is {height}
+        </p>
+    </diiv>
+}
