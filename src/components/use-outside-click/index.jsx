50441f46dce56f5ba9d87e2cd2e1585cf9a81dad
Katie Ludwig <katierludwig@gmail.com>
2025-11-03 14:51:33 -0800

    wip

--- src/components/use-outside-click/index.jsx
+++ src/components/use-outside-click/index.jsx
@@ -1,15 +1,23 @@
 import { useEffect } from "react";
 
 export default function useOutsideClick(ref, handler) {
-    
+
     useEffect(() => {
 
         function listener(event) {
             if (!ref.current || ref.current.contains(event.target)) {
-                return
+                return;
             }
 
             handler(event)
         }
-    }, [handler,ref])
+
+        document.addEventListener('mousedown', listener);
+        document.addEventListener('touchstart', listener);
+
+        return () => {
+            document.removeEventListener('mousedown', listener);
+            document.removeEventListener('touchstart', listener);
+        }
+    }, [handler, ref])
 }
